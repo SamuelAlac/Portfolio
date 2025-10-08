9f7f892406d3358899aa16eb558d05bddf88543f
SamuelAlac <samuelalac21@gmail.com>
2025-10-08 09:57:46 +0800

    update: Contact info

--- frontend/src/lib/constants/socmeds.ts
+++ frontend/src/lib/constants/socmeds.ts
@@ -1,1 +1,24 @@
 export const socmeds = ['/linkedin.svg', '/github.svg', '/facebook.svg', '/instagram.svg']
+
+export const socials = [
+    {
+        name: 'LinkedIn',
+        img: '/linkedin.svg',
+        link: 'https://www.linkedin.com/in/samuel-alac-7a574534b/',
+    },
+    {
+        name: 'GitHub',
+        img: '/github.svg',
+        link: 'https://github.com/SamuelAlac',
+    },
+    {
+        name: 'Facebook',
+        img: '/facebook.svg',
+        link: 'https://www.facebook.com/samuelonearth13',
+    },
+    {
+        name: 'Instagram',
+        img: '/instagram.svg',
+        link: 'https://www.instagram.com/samuelonearth/',
+    }
+]
--- frontend/src/pages/homepage/components/ContactMe.tsx
+++ frontend/src/pages/homepage/components/ContactMe.tsx
@@ -1,4 +1,5 @@
-import { socmeds } from "@/lib/constants/socmeds"
+import { socials } from "@/lib/constants/socmeds"
+import { Link } from "react-router-dom"
 
 export const ContactMe = () => {
   return (
@@ -6,8 +7,10 @@ export const ContactMe = () => {
         <div className="flex flex-col gap-6 p-3">
           <h1 className="text-3xl">Contact Me</h1>
           <div className="flex flex-row gap-8">
-            {socmeds.map((socmed, index) =>(
-                <img src={socmed} alt="" className="h-8" />
+            {socials.map((social, index) =>(
+              <Link to={social.link} target="_blank" className="transition ease-in-out hover:scale-125">
+                <img key={index} src={social.img} alt={social.name} title={social.link} className="h-8" />
+              </Link>
             ))}
           </div>
         </div>
